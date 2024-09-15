08d820232335ea535b953062d2578566f21576b1
NKRI <nkri.dev@gmail.com>
2024-09-15 11:57:04 +0200

    search button

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("test");
+//search bar
+const searchBar = document.querySelector(".find-location button");
+
+searchBar.addEventListener("click", e =>{
+    e.preventDefault();
+
+    const valueCity = document.querySelector(".find-location input").value;
+
+    if(valueCity == ""){
+        alert("Please, put a city name");
+        return;
+    }
+
+    
+    console.log(valueCity);
+});
+
