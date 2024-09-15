40d8bb74ae5530f05046f3b6814912aca073ce55
NKRI <nkri.dev@gmail.com>
2024-09-15 19:08:37 +0200

    add get api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //search bar
 const searchBar = document.querySelector(".find-location button");
 
-searchBar.addEventListener("click", e =>{
+searchBar.addEventListener("click", async e =>{
     e.preventDefault();
 
     const valueCity = document.querySelector(".find-location input").value;
@@ -11,7 +11,21 @@ searchBar.addEventListener("click", e =>{
         return;
     }
 
-    
-    console.log(valueCity);
+    //call my API (Weather API)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=5e9889ccdfe8d1f2d98aad805b64d7ad")
+    .then(responce => {
+        if(!responce.ok){
+            //TODO : city is not found
+            alert("City not found");
+        }
+        return responce.json();
+    })
+    .then(datas => {
+        console.log(datas.main.temp);
+    })
+    .catch(errer =>{
+        console.error("Problème avec la météo: " + errer);
+        alert("City not found");
+    });
 });
 
